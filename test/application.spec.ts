0a13e8d13765ed06712cffc2993b7b707d4fa279
mark.down <mark.down@kakaocorp.com>
2023-01-14 15:13:15 +0900

    라우터 테스트

--- test/application.spec.ts
+++ test/application.spec.ts
@@ -10,4 +10,35 @@ describe('express 라우터 테스트', () => {
 
     expect(res.body).toEqual({ message: 'Hello world!' });
   });
+
+  describe('/cats 라우터 테스트', () => {
+    test('GET /cats 고양이 목록을 가져올 수 있다.', async () => {
+      const res = await request(application.instance)
+        .get('/cats')
+        .expect('Content-Type', /json/);
+
+      expect(res.body.cats).toHaveLength(2);
+    });
+
+    test('GET /cats/Tom 이름이 Tom인 고양이를 가져올 수 있다.', async () => {
+      const res = await request(application.instance)
+        .get('/cats/Tom')
+        .expect('Content-Type', /json/);
+
+      expect(res.body.cat).toEqual({ name: 'Tom' });
+    });
+
+    test('POST /cats Dave라는 고양이를 추가할 수 있다.', async () => {
+      await request(application.instance)
+        .post('/cats')
+        .send({ name: 'Dave' })
+        .expect(204);
+
+      const res = await request(application.instance)
+        .get('/cats/Dave')
+        .expect('Content-Type', /json/);
+
+      expect(res.body.cat).toEqual({ name: 'Dave' });
+    });
+  });
 });
