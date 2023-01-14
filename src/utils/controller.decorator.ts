6055ef3ea4f64c17b77a62d742050d9392590a33
mark.down <mark.down@kakaocorp.com>
2023-01-14 14:50:19 +0900

    데코레이터 구현

---
+++ src/utils/controller.decorator.ts
@@ -0,0 +1,7 @@
+import { MetadataKeys } from './metadata.key';
+
+export const Controller = (basePath: string): ClassDecorator => {
+  return (target) => {
+    Reflect.defineMetadata(MetadataKeys.BASE_PATH, basePath, target);
+  };
+};
---
+++ src/utils/handlers.decorator.ts
@@ -0,0 +1,38 @@
+import { MetadataKeys } from './metadata.key';
+
+export enum Methods {
+  GET = 'get',
+  POST = 'post',
+}
+
+export interface IRouter {
+  method: Methods;
+  path: string;
+  handlerName: string | symbol;
+}
+
+const methodDecoratorFactory = (method: Methods) => {
+  return (path: string): MethodDecorator => {
+    return (target, propertyKey) => {
+      const controllerClass = target.constructor;
+
+      const routers: IRouter[] = Reflect.hasMetadata(
+        MetadataKeys.ROUTERS,
+        controllerClass
+      )
+        ? Reflect.getMetadata(MetadataKeys.ROUTERS, controllerClass)
+        : [];
+
+      routers.push({
+        method,
+        path,
+        handlerName: propertyKey,
+      });
+
+      Reflect.defineMetadata(MetadataKeys.ROUTERS, routers, controllerClass);
+    };
+  };
+};
+
+export const Get = methodDecoratorFactory(Methods.GET);
+export const Post = methodDecoratorFactory(Methods.POST);
